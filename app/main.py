8aa33c5a8c336a1af8049e30714e46d898fe4f88
Rdx-ai-art <direndertemp@gmail.com>
2026-01-18 17:04:05 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -209,3 +209,4 @@ if __name__ == "__main__":
     parser.add_argument("--port", type=int, default=8000)
     args = parser.parse_args()
     uvicorn.run(app, host=args.host, port=args.port, access_log=False)
+    
